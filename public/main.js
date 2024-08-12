85521754313d0ce58c9b4d6a8fc5a98eb37b851c
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-12 10:25:38 +0300

    set the typeIy library and type the heading, add animations

--- public/main.js
+++ public/main.js
@@ -24,3 +24,27 @@ axios.get('http://localhost:3000/user-info')
         }
     }
 })
+
+//heading script
+new TypeIt("#testText", {
+    speed: 50,
+    startDelay: 900,
+  })
+    .type("ello, I", { delay: 100 })
+    .move(-7, { delay: 100 })
+    .type("h", { delay: 400 })
+    .move(null, { to: "START", instant: true, delay: 300 })
+    .move(1, { delay: 200 })
+    .delete(1)
+    .type("H", { delay: 225 })
+    .pause(200)
+    .move(2, { instant: true })
+    .pause(200)
+    .move(5, { instant: true })
+    .type("'", { delay: 350 })
+    .type("m Yana Zubal")
+    .delete(10)
+    .type('<em><strong class="font-semibold">Yana Zubal.</strong></em>', {
+      speed: 100,
+    })
+    .go();
